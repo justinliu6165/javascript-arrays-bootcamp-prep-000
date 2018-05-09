39fbc3a31ecab524403587e23a3ddc43e0d2d502
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-09 17:11:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, element){
-  return [[element], arr[0],...]
+  return [...,[element], arr[0],]
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, element) {
