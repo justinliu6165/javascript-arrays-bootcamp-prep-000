9fa50fdbfd240cca60529f6131612aec5fb9d052
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-09 17:10:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(arr, element) {
 }
 
 function addElementToTheEndOfArray(arr,element) {
-  
+  return [arr, element, ...]
 }
 
 function destructivelyAddElementToEndOfArray(arr,element){
