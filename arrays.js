d499208554ea3fb0bba6a504380785814e76ada8
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-09 16:49:33 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(arr){
+  var array = "";
+  for (i=0; i<=arr.length; i++){
+    arr.unshift(array[i]);
+  }
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr) {
+  
+}
